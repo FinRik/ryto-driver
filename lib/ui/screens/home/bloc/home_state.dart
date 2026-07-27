8f7fbb087d0014afe114aa1609e75933a2bf1dd0
Praise650 <ayokanmi1275@gmail.com>
2026-07-27 12:13:25 +0100

    fix: homepage failing on iOS

--- lib/ui/screens/home/bloc/home_state.dart
+++ lib/ui/screens/home/bloc/home_state.dart
@@ -2,14 +2,17 @@ part of 'home_bloc.dart';
 
 enum HomeStatus { initial, loading, success, failure }
 enum OnboardingFetchStatus { initial, loading, success, failure }
+enum TripsFetchStatus { initial, loading, success, failure }
 
 class HomeState extends Equatable {
   final HomeStatus status;
   final OnboardingFetchStatus onboardingStatus;
+  final TripsFetchStatus tripsStatus;
   final int? tripsCount;
   final WalletSummary? dailyEarnings;
   final List<Trip>? currentTrips;
   final String? errorMessage;
+  final String? tripsErrorMessage;
 
   // onboarding status
   final ActionStatus verification;
@@ -21,10 +24,12 @@ class HomeState extends Equatable {
   const HomeState({
     this.status = HomeStatus.initial,
     this.onboardingStatus = OnboardingFetchStatus.initial,
+    this.tripsStatus = TripsFetchStatus.initial,
     this.tripsCount,
     this.dailyEarnings,
     this.currentTrips,
     this.errorMessage,
+    this.tripsErrorMessage,
 
     // onboarding status
     this.verification = ActionStatus.notSet,
@@ -50,10 +55,12 @@ class HomeState extends Equatable {
   HomeState copyWith({
     HomeStatus? status,
     OnboardingFetchStatus? onboardingStatus,
+    TripsFetchStatus? tripsStatus,
     int? tripsCount,
     WalletSummary? dailyEarnings,
     List<Trip>? currentTrips,
     String? errorMessage,
+    String? tripsErrorMessage,
 
     ActionStatus? verification,
     ActionStatus? vehicle,
@@ -64,10 +71,12 @@ class HomeState extends Equatable {
     return HomeState(
       status: status ?? this.status,
       onboardingStatus: onboardingStatus ?? this.onboardingStatus,
+      tripsStatus: tripsStatus ?? this.tripsStatus,
       tripsCount: tripsCount ?? this.tripsCount,
       dailyEarnings: dailyEarnings ?? this.dailyEarnings,
       currentTrips: currentTrips ?? this.currentTrips,
       errorMessage: errorMessage ?? this.errorMessage,
+      tripsErrorMessage: tripsErrorMessage ?? this.tripsErrorMessage,
 
       // onboarding status
       verification: verification ?? this.verification,
@@ -82,10 +91,12 @@ class HomeState extends Equatable {
   List<Object?> get props => [
     status,
     onboardingStatus,
+    tripsStatus,
     tripsCount,
     dailyEarnings,
     currentTrips,
     errorMessage,
+    tripsErrorMessage,
     verification,
     vehicle,
     background,
